7dfe9cf87abe5d3b8326f726cdbe8892c4b2bb1e
Moya280400 <stevenmoyita28gmail.com>
2020-10-15 21:49:22 -0600

    Modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { ShareModule } from './share/share.module';
+import { HomeModule } from './home/home.module';
+import { UsuarioModule } from './usuario/usuario.module';
+import { VideojuegoModule } from './videojuego/videojuego.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule,
+    ShareModule,
+    HomeModule,
+    VideojuegoModule,
+    UsuarioModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/videojuego/videojuego.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { VideojuegoRoutingModule } from './videojuego-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    VideojuegoRoutingModule
+  ]
+})
+export class VideojuegoModule { }
